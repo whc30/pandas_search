11c4ced008722edf236a519e83701c5c839396e7
Will Clare <email@address.com>
2021-12-27 18:06:57 +0000

    Made more general - can now apply to any iterable.

--- pdsearch.py
+++ pdsearch.py
@@ -1,16 +1,17 @@
-import pandas as pd
 
-def search(df,name,**kwargs):
-    '''Search the columns of a Pandas dataframe `df` for columnname `name`.
+
+#TODO - change name and docs to make more general - this can apply for any object
+def search(it,name,**kwargs):
+    '''Search an iterable `it` for name `name`. This is commonly used to search the columns of Pandas DataFrames.
     Toggle case-sensitivity with `case` (default=False).
     Toggle removing underscores with `remove_us` (default=False).'''
 
     #Check input types
-    if not isinstance(df,pd.DataFrame):
-        raise TypeError("Expected type for `df` input is a Pandas.DataFrame, got {}".format(type(df)))
-    if type(name) == float or type(name) == int:
-        name = str(name) #try, except?
-    if type(name) != str:
+    it_names_orig = list(it)
+
+    try:
+        name = str(name)
+    except:
         raise TypeError("Expected type for `name` input is str, got {}".format(type(name)))
     
     #Extra options through the kwargs
@@ -22,21 +23,25 @@ def search(df,name,**kwargs):
         rus = False
         
     #Now the function
-    df_cols_orig = list(df.columns)
     if not case:
-        df_cols = [col.lower() for col in df.columns] #Make them lower case for easier checking
+        it_names = [col.lower() for col in it_names_orig] #Make them lower case for easier checking
         name = name.lower() #Make the searched field lower case
     else:
-        df_cols = [col for col in df.columns]
+        it_names = [col for col in it_names_orig]
     if rus:
-        df_cols = [col.replace('_','') for col in df_cols]
+        it_names = [col.replace('_','') for col in it_names]
     
-    mapping = dict(zip(df_cols,df_cols_orig)) #A mapping for referring back to the original
+    mapping = dict(zip(it_names,it_names_orig)) #A mapping for referring back to the original
     
-    cols = [mapping[col] for col in df_cols if name in col]
+    names = [mapping[col] for col in it_names if name in col]
 
-    return cols
+    return names
 
 if __name__ == '__main__':
+    import pandas as pd
+
     df = pd.DataFrame({'A':[1,2,3],'b':[2,3,4],'ab3':[55,55,33]})
     print(search(df,'a'))
+
+    listt = ['a','b','veg']
+    print(search(listt,'v'))
